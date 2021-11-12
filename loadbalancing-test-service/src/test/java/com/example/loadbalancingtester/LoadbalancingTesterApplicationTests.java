339fc2254e76ace8490b04ae8cdb592987f93f98
MiPf0 <michaelpfeiffer@gmx.at>
2021-11-12 22:36:23 +0100

    added load balancing test service

---
+++ loadbalancing-test-service/src/test/java/com/example/loadbalancingtester/LoadbalancingTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loadbalancingtester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadbalancingTesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
